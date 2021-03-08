5fcad2101a629b0ee6ec2e1b82d0953c87d5efbe
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-03-07 18:23:10 -0700

    further boolean/integer testing

--- data/migrations/20210307212841_initial.js
+++ data/migrations/20210307212841_initial.js
@@ -4,7 +4,7 @@ exports.up = async function(knex) {
 		table.increments("project_id")
 		table.string("project_name").notNull()
 		table.text("project_description")
-		table.boolean("project_completed")
+		table.integer("project_completed").default(0)
   })
 
 	await knex.schema.createTable("resources", (table) => {
@@ -17,7 +17,7 @@ exports.up = async function(knex) {
 		table.increments("task_id")
 		table.text("task_description").notNull()
 		table.text("task_notes")
-		table.boolean("task_completed")
+		table.integer("task_completed").default(0)
 		table.integer("project_id")
 			.notNull()
 			.references("project_id")
